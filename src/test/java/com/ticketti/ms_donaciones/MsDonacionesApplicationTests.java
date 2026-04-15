6565318c74dd794e4bdd3a9940a560402b65f71a
marceloriv <ma.riveracontreras@duocuc.cl>
2026-04-15 17:49:31 -0400

    feat: :tada: primer commit

---
+++ src/test/java/com/ticketti/ms_donaciones/MsDonacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketti.ms_donaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsDonacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
