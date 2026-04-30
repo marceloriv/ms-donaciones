92f962e94f8be419511c14864c481d873e5ee2ac
Pamela Alvarez <palvarezlara@gmail.com>
2026-04-30 00:58:56 -0400

    chore(donaciones): estructura inicial del microservicio

--- src/main/java/com/ticketti/ms_donaciones/MsDonacionesApplication.java
+++ src/main/java/com/ticketti/ms_donaciones/MsDonacionesApplication.java
@@ -2,11 +2,19 @@ package com.ticketti.ms_donaciones;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * Microservicio MS-Donaciones.
+ * Registra y consulta las donaciones del 10% por compra
+ * destinadas a las organizaciones beneficiarias.
+ */
 @SpringBootApplication
+@EnableDiscoveryClient // se registra en Eureka
 public class MsDonacionesApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MsDonacionesApplication.class, args);
 	}
 
